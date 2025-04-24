f694696481c4edb12fdea070b0cb347cbc5fae6e
Yulia Light <kitty@lightside.games>
2025-04-25 01:58:13 +0400

    Tutorial❤️

---
+++ Assets/Scripts/Core/Tutorial.cs
@@ -0,0 +1,36 @@
+﻿using DG.Tweening;
+using LSCore;
+using LSCore.ConfigModule;
+using UnityEngine;
+
+public class Tutorial : MonoBehaviour
+{
+    public Spawner spawner;
+    public Transform pointer;
+    private int currentFingerId;
+    private LSInput.Simulator.Touch touch;
+    
+    private void Start()
+    {
+        if (FirstTime.IsNot("Tutorial", out var pass))
+        {
+            LSInput.IsManualControl = true;
+            pointer.position = spawner.transform.position;
+            pointer.DOMove(Vector3.zero, 1f).SetLoops(-1, LoopType.Restart).OnStart(TouchDown).OnStepComplete(TouchDown);
+        }
+    }
+
+    private void Update()
+    {
+        if (LSInput.TouchCount > 0)
+        {
+            touch.position = Camera.main.WorldToScreenPoint(pointer.position);
+        }
+    }
+
+    private void TouchDown()
+    {
+        touch?.Release();
+        touch = LSInput.Simulator.TouchDown(Camera.main.WorldToScreenPoint(pointer.position));
+    }
+}
