976f2ec7f7aac0c3ab0d19386f534149ef1429fe
CkBupTaJI <fgmtfit@yandex.ru>
2025-04-30 18:24:33 +1000

    Iproved UI and add destroy anim 🐺

--- Assets/Scripts/Core/FieldAnimator/FieldAnimator.Marmalade.cs
+++ Assets/Scripts/Core/FieldAnimator/FieldAnimator.Marmalade.cs
@@ -1,3 +1,5 @@
+using System.Collections.Generic;
+using DG.Tweening;
 using UnityEngine;
 
 namespace Core
@@ -6,12 +8,34 @@ namespace Core
     {
         public class Marmalade : Handler
         {
+            [SerializeField] public float probabilityEffect = 0.5f;
+            public AnimationCurve animationCurve;
             public ParticleSystem fx;
+            public HashSet<Vector2Int> blocksToExplode = new();
             public override void Handle()
             {
+                foreach (var (index, _) in fieldManager.UniqueSuicidesData)
+                {
+                    if (UnityEngine.Random.value < probabilityEffect)
+                    {
+                        blocksToExplode.Add(index);
+                    }
+                }
                 foreach (var (index, block) in fieldManager.UniqueSuicidesData)
                 {
-                    Destroy(block.gameObject);
+                    var seq = DOTween.Sequence();
+                    var tr = block.transform;
+
+                    seq.Append(tr.DOScale(0, 1f).SetEase(animationCurve));
+                    seq.OnComplete(() =>
+                    {
+                        if (blocksToExplode.Contains(index))
+                        {
+                            
+                            Instantiate(fx, block.transform.position, fx.transform.rotation);
+                        }
+                        Destroy(block.gameObject);
+                    });
                 }
             }
         }
